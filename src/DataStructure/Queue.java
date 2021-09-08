6d308daf79e698e712a63b571e3bb3ac0fbc89de
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-09-08 15:44:07 +0100

    feat(DS): add Queue

---
+++ src/DataStructure/Queue.java
@@ -0,0 +1,30 @@
+package DataStructure;
+
+public class Queue<T>
+{
+    private LinkedList<T> _list;
+
+    public void enqueue(T item)
+    {
+        if (_list == null)
+        {
+            _list = new LinkedList<>(item);
+            return;
+        }
+        _list.append(item);
+    }
+
+    public T dequeue()
+    {
+        if (_list == null)
+            return null;
+        T r = _list.getData();
+        _list = _list.next();
+        return r;
+    }
+
+    public void clear()
+    {
+        _list = null;
+    }
+}
